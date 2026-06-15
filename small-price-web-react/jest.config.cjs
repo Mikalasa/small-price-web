a277ecd785cfa7d2de728b09c4af11f963decaf3
gezhe <44009421+Mikalasa@users.noreply.github.com>
2026-06-15 20:55:00 +1200

    setup cypress and male project clean

--- small-price-web-react/jest.config.cjs
+++ small-price-web-react/jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   testMatch: ["<rootDir>/tests/unit/**/*.test.js"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/tests/unit/setupTests.js"],
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest",
   },
